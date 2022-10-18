a96cb5b0cedf3c7358c48beb7b3d49ccd287d9bd
Souhail Ouni <Souhail.Ouni@lombardrisk.com>
2022-10-18 11:05:43 +0100

    full-angular-app-part2

---
+++ src/app/allcountries/allcountries.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-allcountries',
+  templateUrl: './allcountries.component.html',
+  styleUrls: ['./allcountries.component.css']
+})
+export class AllcountriesComponent implements OnChanges {
+  @Input() allCountriesData: any;
+  constructor() { }
+
+  ngOnChanges() {
+    this.allCountriesData.sort((a: any, b: any)=> {
+      return b.TotalConfirmed - a.TotalConfirmed;
+    });
+    // console.log(this.allCountriesData);
+
+  }
+
+}
